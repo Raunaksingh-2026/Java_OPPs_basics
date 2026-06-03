d5bd8fa573107d84fe8a9e408002f50779782a47
Raunaksingh-2026 <rajputraunaksingh23082005@gmail.com>
2026-06-03 20:51:13 +0530

    Updated OOPs_4_1_Polymorphism.java File

--- src/pkg2/OOPs_4_1_Polymorphism.java
+++ src/pkg2/OOPs_4_1_Polymorphism.java
@@ -9,7 +9,7 @@ Types -> 1. Compile-Time Polymorphism -> Method Overloading
 // PARENT CLASS / SUPER CLASS -> Provides a common method that child classes can override.
 class Parent {
 
-//     Method that displays the role of a Parent
+//    Method that displays the role of a Parent
     void role() {
         System.out.println("I am a Parent.");
     }
@@ -20,46 +20,56 @@ class Child1 extends Parent {
 
     public Child1() {
     }
-    /*
-    @Override -> Tells compiler that this method is intended to override a method from the parent class.
-    Benefit -> Compiler checks for mistakes.
-    */
+
+//    @Override -> Tells compiler that this method is intended to override a method from the parent class.
+//    Benefit -> Compiler checks for mistakes.
     @Override // form of metadata that provide supplemental information about a program without being part of the program's actual logic.
     void role() { // METHOD OVERRIDING -> Same method name | Same parameters | Same return type || BUT || Different implementation.
         System.out.println("I am a First Child.");
     }
-
 }
 
 // Derived class "Child2" that Inherits Parent class and overrides the role method "Parent"
 class Child2 extends Parent {
 
-    // Overridden method to show the role of a "Child2"
+//    Overridden method to show the role of a "Child2"
+//    METHOD OVERRIDING -> Same method name | Same parameters | Same return type -> But Different implementation.
     void role() {
         System.out.println("I am a Second Child.");
     }
 }
 
-public class OPPs_4_1_Polymorphism {
+public class OOPs_4_1_Polymorphism {
+/*
+    DYNAMIC METHOD DISPATCH -> Parent reference -> Child object
+    -> Method call is decided at Runtime based on actual object type.
+        Reference Type : Parent
+        Object Type    : Child1 / Child2
+    -> This is the foundation of Runtime Polymorphism.
+*/
     public static void main(String[] args) {
         /***************************** DYNAMIC METHOD DISPATCH **************************/
-        // Creating a reference of type "Parent" but initializing it with "Parent" class object
-        //At runtime, the method that gets executed depends on the actual object type, not the reference type.
-        Parent familyMember = new Parent();
-
+//        Creating a reference of type "Parent" but initializing it with "Parent" class object
+//        At runtime, the method that gets executed depends on the actual object type, not the reference type.
+        Parent familyMember = new Parent(); //  Parent Reference | Parent Object -> Calls Parent's role() method.
         familyMember.role();// Calling the role method of "Parent"
 
-        // Creating a reference of type "Parent" but initializing it with "Child1" class object
+/*
+        Parent Reference | Child1 Object -> Upcasting happens automatically.
+        -> At runtime JVM checks: Actual object = Child1
+        >> Therefore Child1's role() method is executed.
+*/
+//        Creating a reference of type "Parent" but initializing it with "Child1" class object
         familyMember = new Child1();
-
         familyMember.role();// Calling the role method. It calls the overridden version in "Child1" class
 
-        // Creating a reference of type "Parent" but initializing it with "Child2" class object
+/*
+        Same reference variable | Different object assigned.
+        -> JVM calls Child2's overridden method at runtime.
+*/
+//        Creating a reference of type "Parent" but initializing it with "Child2" class object
         familyMember = new Child2();
-
         familyMember.role();// Calling the role method. It calls the overridden version in "Child2" class
-
-
     }
 }
 
