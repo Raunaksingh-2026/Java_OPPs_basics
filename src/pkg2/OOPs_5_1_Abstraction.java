ec1a22ed0c3a5354d09394cf6f179c74ff095f98
Raunaksingh-2026 <rajputraunaksingh23082005@gmail.com>
2026-06-04 22:53:08 +0530

    Added Multilevel Inheritance With Abstraction

--- src/pkg2/OOPs_5_1_Abstraction.java
+++ src/pkg2/OOPs_5_1_Abstraction.java
@@ -27,8 +27,14 @@ abstract class Car {
 /******************* SINGLE LEVEL INHERITANCE with ABSTRACTION *******************/
 //    CHILD CLASS of ABSTRACT CLASS [ "Car" CLASS ] must have body of all ABSTRACT METHOD in SINGLE CLASS which was declared in PARENT CLASS
 //    as many CHILD CLASS of ABSTRACT CLASS present then every CHILD CLASS must have all ABSTRACT METHOD
-
-
+/*
+---------------------
+        Car
+          ↓
+        WagonR
+---------------------
+    --> WagonR must implement ALL abstract methods of Car.
+*/
 class WagonR extends Car {
     //    providing implementation for ABSTRACT METHOD
     public void driveCar() {
@@ -40,16 +46,62 @@ class WagonR extends Car {
     }
 }
 
+/******************* MULTI LEVEL INHERITANCE with ABSTRACTION *******************/
+//    CHILD CLASS of "Car" CLASS must have body of ABSTRACT METHOD in CLASS which was declared in PARENT CLASS
+//    if we want to add few METHOD only then we have to make this as ABSTRACT CLASS and remaining METHODS will be initialized in another CLASS which INHERIT this ABSTRACT Class
+/*
+----------------------------
+             Car
+              ↓
+    Lamborghini (Abstract)
+              ↓
+    ModifiedLamborghini
+----------------------------
+    -> An abstract child class can implement SOME methods and leave remaining methods for another child.
+ */
+abstract class Lamborghini extends Car {
+
+//    providing implementation for ABSTRACT METHOD
+    public void driveCar() {
+        System.out.println("Ready to Drive...");
+    }
+    public String colorOfLamborghini(String color) {
+        return "This is " + color + " Color";
+    }
+}
+
+//    creating CONCRETE CLASS
+//    remaining ABSTRACT method will be initialized here
+class ModifiedLamborghini extends Lamborghini {
+
+    //    providing implementation for another remaining ABSTRACT METHOD
+    public void speedOfCar(int speed) {
+        System.out.println(speed + " km/hr");
+    }
+}
 public class OOPs_5_1_Abstraction {
     public static void main(String[] args) {
 
     /**----------------------------------- ABSTRACTION -----------------------------------**/
         /**++++++++++++++++ SINGLE LEVEL INHERITANCE with ABSTRACTION ++++++++++++++++**/
+        System.out.println("<-- By using SINGLE LEVEL INHERITANCE with ABSTRACTION -->");
 //        we cannot create object of ABSTRACT CLASS
 //        Car ability = new Car(); // this will go to give a compile time error because wo only use ABSTRACT CLASS as REFERENCE only not for OBJECT
         Car ability = new WagonR();
         ability.playMusic();
         ability.driveCar();
         ability.speedOfCar(40);
+
+        /**++++++++++++++ Multi LEVEL INHERITANCE with ABSTRACTION +++++++++++++++++**/
+        System.out.println("<-- By using MULTI LEVEL INHERITANCE with ABSTRACTION -->");
+//        Car car = new Car(); // this will go to give a compile time error because wo only use ABSTRACT CLASS as REFERENCE only not for OBJECT
+//        Car carLamborghini = new Lamborghini(); // this will also go to give a compile time error because wo only use ABSTRACT CLASS as REFERENCE only not for OBJECT
+        Car carModifiedLamborghini = new ModifiedLamborghini();
+        carModifiedLamborghini.playMusic();
+        carModifiedLamborghini.driveCar();
+        carModifiedLamborghini.speedOfCar(80);
+
+        Lamborghini lamborghini = new ModifiedLamborghini(); // to use CONCRETE METHOD of "Lamborghini" ABSTRACT CLASS we have used this ClASS as REFERENCE and make OBJECT to Its CHILD CONCRETE CLASS
+        System.out.println(lamborghini.colorOfLamborghini("Red"));
     }
 }
